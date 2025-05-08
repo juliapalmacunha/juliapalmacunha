c0a544dac61ec6d272349d241208a29750be7cd6
Julia Palma Cunha <159209450+juliapalmacunha@users.noreply.github.com>
2025-05-08 09:24:01 -0300

    correção

--- src/pages/PageApresentacao.jsx
+++ src/pages/PageApresentacao.jsx
@@ -9,29 +9,29 @@ import EnglobaTitulo from '../componentes/EnglobaTitulo';
 
 const Apresentacao = () => {
   const logos = [
-    { nome: "HTML"},
-    { nome: "CSS"},
+    { nome: "HTML" },
+    { nome: "CSS" },
     { nome: "JavaScript" },
-    { nome: "React"},
+    { nome: "React" },
     { nome: "TypeScript" },
     { nome: "C++" },
-    { nome: "TailwindCSS"},
+    { nome: "TailwindCSS" },
     { nome: "Styled Components" },
     { nome: "Material UI" },
-    { nome: "Node.js"},
+    { nome: "Node.js" },
     { nome: "Firebase" },
     { nome: "Git" },
     { nome: "GitHub" }
-    
+
   ];
 
   return (
 
-    <section 
-      className= " lg:gap-[10px]  flex-wrap lg:flex lg:w-full justify-start text-white p-[5px] font-poppinsRegular  items-center  w-full m-[5px]"
+    <section
+      className=" lg:gap-[10px]  flex-wrap lg:flex lg:w-full justify-start text-white p-[5px] font-poppinsRegular  items-center  w-full m-[5px]"
       id="sobre"
     >
-     
+
       <motion.div
         initial={{ opacity: 0, x: -100 }}
         whileInView={{ opacity: 1, x: 0 }}
@@ -43,12 +43,14 @@ const Apresentacao = () => {
         </EnglobaTitulo>
 
         <div className="lg:w-[50%] text-justify">
-          <p>Sou uma entusiasta da tecnologia, dedicada a transformar linhas de código em soluções inovadoras. Atualmente graduanda no curso de Ciencias & Tecnologia e estudos voltados para o desenvolvimento de aplicações web. Busco impulsionar o progresso tecnológico, almejando criar impacto positivo por meio da programação. Aqui estão algumas tecnologias com as quais estou familiarizada:</p>
-          
+          <p>Sou estudante de Ciências & Tecnologia, focada no desenvolvimento de aplicações web. Estou sempre buscando entender o "porquê" das coisas e criar soluções práticas e eficientes. Em meus projetos, como simuladores e ferramentas interativas, exploro tecnologias como React, Firebase e JavaScript.
+
+            Estou em constante evolução e aberta a novos desafios que me ajudem a crescer como programadora. Se você busca alguém com curiosidade, dedicação e capacidade de resolver problemas, podemos ter uma boa conversa.. Aqui estão algumas tecnologias com as quais estou familiarizada:</p>
+
         </div>
       </motion.div>
 
-    
+
       <motion.div
         className="h-full"
         initial={{ opacity: 0, x: 100 }}
@@ -58,8 +60,8 @@ const Apresentacao = () => {
       >
         <div className="flex flex-wrap p-[10px] justify-start pt-12 gap-[12px] lg:w-[50%] ">
           {logos.map((logo, index) => (
-            <motion.div 
-              key={logo.nome} 
+            <motion.div
+              key={logo.nome}
               className="flex mt-[0px] mb-[0px]"
               initial={{ opacity: 0, x: 50 }}
               whileInView={{ opacity: 1, x: 0 }}
@@ -74,8 +76,8 @@ const Apresentacao = () => {
           ))}
         </div>
 
-        
-        <motion.div 
+
+        <motion.div
           className="h-[150px] ml-[10px]"
           initial={{ opacity: 0 }}
           whileInView={{ opacity: 1 }}
@@ -88,11 +90,11 @@ const Apresentacao = () => {
             <a href="/Julia Palma Cunha CurriculoDev.pdf" download className="relative inline-block px-6 py-3 rounded-lg font-bold bg-[#c300ff] text-black overflow-hidden group">
               <span className="absolute inset-0 w-full h-full bg-[#8136b3] transform scale-x-0 group-hover:scale-x-100 origin-left transition-all duration-500 ease-in-out"></span>
               <span className="relative z-40  text-white flex">
-                <FaFileDownload 
-                className='mt-[5px] mr-[10px] '
+                <FaFileDownload
+                  className='mt-[5px] mr-[10px] '
                 />
-                <p>Baixar Currículo</p> 
-                </span>
+                <p>Baixar Currículo</p>
+              </span>
             </a>
           </section>
         </motion.div>
